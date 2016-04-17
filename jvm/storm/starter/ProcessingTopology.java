80cc23845ac6145065a6bc9ca9e59380979fa7c1
gdu <grandture1@gmail.com>
2016-04-16 21:48:20 -0500

    Processing Topology benchmark for latency-based load balance scenario-c

--- jvm/storm/starter/ProcessingTopology.java
+++ jvm/storm/starter/ProcessingTopology.java
@@ -3,7 +3,6 @@
 import storm.starter.bolt.AggregationBolt;
 import storm.starter.bolt.FilterBolt;
 import storm.starter.bolt.TestBolt;
-import storm.starter.bolt.TestBolt1;
 import storm.starter.bolt.TransformBolt;
 import storm.starter.spout.RandomLogSpout;
 import backtype.storm.Config;
@@ -18,21 +17,21 @@ public class ProcessingTopology {
 		TopologyBuilder builder = new TopologyBuilder();
 
 		builder.setSpout("spout_head", new RandomLogSpout(), 5);
-		builder.setBolt("bolt_transform", new TransformBolt(), 4).shuffleGrouping("spout_head").addConfiguration("groupId", "1");
+		builder.setBolt("bolt_transform", new TransformBolt(), 5).shuffleGrouping("spout_head").addConfiguration("groupId", "1");
 
-		builder.setBolt("bolt_filter", new TestBolt(), 4).shuffleGrouping("bolt_transform").addConfiguration("groupId", "2");
-		builder.setBolt("bolt_join1", new TestBolt(), 4).shuffleGrouping("bolt_filter").addConfiguration("groupId", "3");
-		builder.setBolt("bolt_output_1", new TestBolt1(),5).shuffleGrouping("bolt_join1");
+		builder.setBolt("bolt_filter", new TestBolt(), 5).shuffleGrouping("bolt_transform").addConfiguration("groupId", "2");
+		builder.setBolt("bolt_join1", new TestBolt(), 5).shuffleGrouping("bolt_filter").addConfiguration("groupId", "3");
+		builder.setBolt("bolt_output_1", new TestBolt(),5).shuffleGrouping("bolt_join1");
 
-		builder.setBolt("bolt_join2", new TransformBolt(), 4).shuffleGrouping("bolt_transform").addConfiguration("groupId", "4");
-		builder.setBolt("bolt_normalize", new TestBolt(), 4).shuffleGrouping("bolt_join2").addConfiguration("groupId", "5");
-		builder.setBolt("bolt_output_2", new TestBolt1(), 5).shuffleGrouping("bolt_normalize");
+		builder.setBolt("bolt_join2", new TransformBolt(), 5).shuffleGrouping("bolt_transform").addConfiguration("groupId", "4");
+		builder.setBolt("bolt_normalize", new TestBolt(), 5).shuffleGrouping("bolt_join2").addConfiguration("groupId", "5");
+		builder.setBolt("bolt_output_2", new TestBolt(), 5).shuffleGrouping("bolt_normalize");
 
 		Config conf = new Config();
     	conf.setDebug(false);
     	conf.setStatsSampleRate(1); // each message is tracked//
-    	
-    	//conf.setImprovedConcurrencyModel(true);
+    	conf.setIShuffleGroupingEnable(1); //0:default, 1:latency-based load balance, 2: the Power Of Two Choice
+    	conf.setIShuffleGroupingAgingRate(0.5);
 		
 		conf.setNumWorkers(5);
 		StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
